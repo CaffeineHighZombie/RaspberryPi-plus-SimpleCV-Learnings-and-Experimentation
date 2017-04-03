3c7ea8aa06fe269d3c5f33c3d408aa21367d4b73
Saranyaraj Rajendran <rs.saranyaraj@gmail.com>
2017-04-03 12:07:15 +0000

    Add MotionExample.py based on sightmachine's PyCon presentation example.

---
+++ MotionExample.py
@@ -0,0 +1,13 @@
+from SimpleCV import Camera, Display, Color
+scaler = 0.5
+cam = Camera()
+disp = Display((640, 480))
+last = cam.getImage().scale(scaler)
+sz = last.width/10
+while disp.isNotDone():
+	img = cam.getImage().scale(scaler)
+	motion = img.findMotion(last,sz,method='HS')
+	motion.draw(color=Color.RED,width=3)
+	img.save(disp)
+	last = img
+
